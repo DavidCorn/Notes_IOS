edc1ad6f5865ed2cd78664d46356dc5812e65773
Qiyao Qin <qiyaoq@uber.com>
2016-07-04 17:40:16 -0700

    delete and revise

--- Podfile
+++
@@ -1,7 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-
-platform :ios, '9.0'
-target 'Notes' do
-  use_frameworks!
-  pod 'Masonry'
-end
--- Pods/Target Support Files/Pods-Notes/Pods-Notes-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_NotesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_NotesVersionString[];
-
